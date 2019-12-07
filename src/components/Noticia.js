b7ef5af6b1995ad41bab82e3e9a60ab3860372b5
Jimmy <bruno_pier@hotmail.com>
2019-12-07 13:39:22 -0300

    Loading images conditionally - Modifying Noticia.js

--- src/components/Noticia.js
+++ src/components/Noticia.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
 const Noticia = ({noticia}) => {
-
+        //Extraer los datos
         const { urlToImage, url, title, description, source } = noticia;
+        //Condicionalmente cargar la imagen si esta disponible
+        const imagen = (urlToImage) ?
+        <div className="card-image">
+            <img src={urlToImage} alt={title} />
+            <span className="card-title">{source.name}</span>
+        </div>
+        : null;
 
     return (
         <div className="col s12 m6 l4">
             <div className="card">
-                <div className="card-image">
-                <img src={urlToImage} alt={title} />
-                </div>
+            {imagen}
                 <div className="card-content">
                     <h3>{title}</h3>
                     <p>{description}</p>
