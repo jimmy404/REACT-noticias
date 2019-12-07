5227a0ca3c141818a2da9008d9bc91e875f7504d
Jimmy <bruno_pier@hotmail.com>
2019-12-07 12:33:25 -0300

    Adding News api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  state ={}
+  state ={
+    noticias: []
+  }
+
+async componentDidMount(){
+  this.consultarNoticias();
+}
+
+  consultarNoticias = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=ar&category=business&apiKey=5bdc8077784142db9ac390ab5b8fd753`;
+    const respuesta = await fetch(url);
+    const noticias = await respuesta.json();
+
+    this.setState({
+      noticias: noticias.articles
+    })
+
+  }
+
   render(){
     return( <h1>Noticias react</h1>
       );
